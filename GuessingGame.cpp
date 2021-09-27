accc071336cda85a1b232130ef7dcc1b835f080d
Nick Peterson <nickpeterson@Nicks-MacBook-Air.local>
2021-09-26 21:20:21 -0700

    final version

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -1,25 +1,39 @@
 # include <iostream>
-
+/*
+Nick Peterson
+9/26/2021
+This program runs a game where the user tries to guess a number 1-100
+upon guessing the right number, it will ask them if they want to play again as well
+as displaying how many attempts it took the to get the correct answer
+*/
 using namespace std;
 
 
 int main()
 {
+  int guesscount = 0;
   while true {
       stillPlaying = true;
-      int randmnum = rand() % 100 + 1; 
+      int randmnum = rand() % 100 + 1;
       int input  = 0;
+      bool stillplaying = true;
       cout << "Enter a number 1-100" << endl;
       cin >> input;
       cout << "You Entered: " << input << endl;
       if input == randnum {
 	  cout << "You guessed the number!" << endl;
-	  cout << "Would You Like to Play Again? y/n" << endl;
-	}
-      else {
-	break;
-      }
+	  cout << "It took you " << guesscount << " tries to guess it!" << endl;
+	  cout << "Would You Like to Play Again? true/false" << endl;
+	  cin >> stillplaying;
+	  if still playing == true {
+	      continue;
+	    }
+	  else {
+	    break
+	      }
       return 0;
-    }
+	}
+      if input != randnum {
+	  guesscount = guesscount + 1;
+	  cout << "You guessed the wrong number!" << endl << "Try again!";
 }
-//delete this
