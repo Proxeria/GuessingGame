458295d1d3ffef5ba9f4d140f1a89ed20d902639
Proxeria <90416414+Proxeria@users.noreply.github.com>
2021-10-03 16:45:30 -0700

    Update GuessingGame.cpp

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -6,34 +6,44 @@ This program runs a game where the user tries to guess a number 1-100
 upon guessing the right number, it will ask them if they want to play again as well
 as displaying how many attempts it took the to get the correct answer
 */
+#include <iostream>
+#include <iomanip>
 using namespace std;
-
+char chr;
 
 int main()
 {
-  int guesscount = 0;
-  while true {
-      stillPlaying = true;
-      int randmnum = rand() % 100 + 1;
-      int input  = 0;
-      bool stillplaying = true;
-      cout << "Enter a number 1-100" << endl;
-      cin >> input;
-      cout << "You Entered: " << input << endl;
-      if input == randnum {
-	  cout << "You guessed the number!" << endl;
-	  cout << "It took you " << guesscount << " tries to guess it!" << endl;
-	  cout << "Would You Like to Play Again? true/false" << endl;
-	  cin >> stillplaying;
-	  if still playing == true {
-	      continue;
-	    }
-	  else {
-	    break
-	      }
-      return 0;
+	int number = 0;
+	int guess = 0;
+	int tries = 0;
+    	char answer;
+
+    srand(number>0);
+	do
+	{
+    number = rand()%100+1;;
+	cout<<"Enter a number between 1 and 100"<<endl;
+	cin>>guess;
+	if (number < guess)
+		cout << "Too high try again" <<endl;
+	tries = 1;
+	} while(number > guess);
+		cout <<"Too low try again" <<endl;
+		tries++;
+	if (number==guess)
+		cout << "You guessed the number! " <<endl;
+	cout << "It took you " << tries << " tries";
+	do
+	{
+cout << "Would you like to play again?  Enter Y/N";
+		cin >> answer;
+    if ('N')
+		cout << "Thanks for playing!";
+	} while(answer='Y');
+			
+
+	
+	cin >> chr;
+		return 0;
+
 	}
-      if input != randnum {
-	  guesscount = guesscount + 1;
-	  cout << "You guessed the wrong number!" << endl << "Try again!";
-}
