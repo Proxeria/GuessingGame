fa71f4f55efa531714b8ac45f4730d15a1c92379
Proxeria <napinpdx@me.com>
2021-10-03 20:47:40 -0400

    Final Edit

--- GuessingGame.cpp
+++ GuessingGame.cpp
@@ -7,42 +7,42 @@ as displaying how many attempts it took the to get the correct answer
 */
 #include <iostream>
 #include <iomanip>
+#include <ctime>
 using namespace std;
 char chr;
 
 int main() {
-	int number = 0;
-	int guess = 0;
-	int tries = 0;
-    	char answer;
-
-    srand(number>0);
-	do
-	{
-    number = rand()%100+1;;
-	cout<<"Enter a number between 1 and 100"<<endl;
-	cin>>guess;
-	if (number < guess)
-		cout << "Too high try again" <<endl;
-	tries = 1;
-	} while(number > guess);
-		cout <<"Too low try again" <<endl;
-		tries++;
-	if (number==guess) {
-		cout << "You guessed the number! " <<endl;
-		cout << "It took you " << tries << " tries";
-	}
-	do
-	{
-		cout << "Would you like to play again?  Enter Y/N";
-		cin >> answer;
-    	if ('N') {
-		cout << "Thanks for playing!";
-	} while(answer='Y');
-			
-
-	
-	cin >> chr;
-		return 0;
-
-	}
+  int number = 0;
+  int guess = 0;
+  int tries = 1;
+  char answer;
+  srand(time(0));
+  do {
+    number = rand()%100+1;
+    do {
+      cout<<"Enter a number between 1 and 100"<<endl;
+      cin>>guess;
+      if (number < guess) {
+	cout << "Too high" <<endl;
+	tries += 1;
+      }
+      if (number > guess) {
+	cout << "Too low" << endl;
+	tries += 1;
+      }
+    } while(guess != number);
+    if (number == guess) {
+      cout << "You guessed the number! " <<endl;
+      cout << "It took you " << tries << " tries" << endl;
+    }
+    
+    cout << "Would you like to play again?  Enter Y/N " << endl;
+    cin >> answer;
+    if (answer == 'N') {
+      cout << "Thanks for playing!" << endl;
+    }
+  } while(answer == 'Y');
+  
+  return 0;
+}
+  
